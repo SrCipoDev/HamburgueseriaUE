f63a76c63c416fa3563a6a0985c4a169dbc79e5b
angel Espino <angel.cipo5@gmail.com>
2024-05-13 20:56:35 +0200

    quitado enlaces de admin sin login

--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -5,9 +5,7 @@
 @section('content')
     <div class="container contenido-principal">
         <div class="row">
-            <h1>Este es su pedido</h1>
-
-            </ul>
+            <h1>Muchas gracias por su confianza, su pedido estará disponible en nuestro local en 30 minutos</h1>
         </div>
     </div>
 @endsection
